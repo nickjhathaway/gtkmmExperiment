a512a996ac19318f0d4790e21af513fb85abf7c2
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-06-23 16:46:56 -0400

    added configure script

---
+++ configure.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import shutil, os, argparse, sys, stat
+sys.path.append("scripts/pyUtils")
+sys.path.append("scripts/setUpScripts")
+from utils import Utils
+from genFuncs import genHelper
+def main():
+    name = "gtkExperimental"
+    libs = "gtkmm"
+    args = genHelper.parseNjhConfigureArgs()
+    cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
+    Utils.run(cmd)
+    
+main()
+
